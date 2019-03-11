0860cf20c86b022a6910228ec0ff3ac9a69ee563
Benjamin Boit <benboit@posteo.de>
2019-03-11 22:02:16 +0100

    fixed userid mapping / added dummy volume to docker-compose

--- docker/php/base/run.sh
+++ docker/php/base/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-MACUSERID=$(ls -ld /var/www | awk '{print $3}')
-usermod -u $MACUSERID 83
-groupmod -g 9999 dialout
-groupmod -g 20 www-data
+LOCALUSERID=$(ls -ld /var/www | awk '{print $3}')
+usermod -u $LOCALUSERID php
+#groupmod -g 9999 dialout
+#groupmod -g 20 www-data
 
 php-fpm7 -F
