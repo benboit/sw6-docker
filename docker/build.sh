5aeb01d1e1bff93c20ab5e04bf2321a1c07473b8
Benjamin Boit <benboit@posteo.de>
2019-04-14 17:26:11 +0200

    added new build step

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,8 @@
 
 docker build -t benboit/sw6-docker-nginx:1.14 nginx && 
 docker build -t benboit/sw6-docker-php:7.2 php/base
+docker build -t benboit/sw6-docker-php:7.2-cli php/cli
 
 docker push benboit/sw6-docker-nginx:1.14
 docker push benboit/sw6-docker-php:7.2
+docker push benboit/sw6-docker-php:7.2-cli
