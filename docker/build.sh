5cee70bb4ca80a36513bf35faaafc33c3d191563
Benjamin Boit <benboit@posteo.de>
2020-04-26 10:20:50 +0200

    corrected build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 docker build -t benboit/sw6-docker-nginx:1.14 nginx && 
-docker build -t benboit/sw6-docker-php:7.2 php/base
-docker build -t benboit/sw6-docker-php:7.2-cli php/cli
+docker build -t benboit/sw6-docker-php:7.3 php/base
+docker build -t benboit/sw6-docker-php:7.3-cli php/cli
 
 docker push benboit/sw6-docker-nginx:1.14
-docker push benboit/sw6-docker-php:7.2
-docker push benboit/sw6-docker-php:7.2-cli
+docker push benboit/sw6-docker-php:7.3
+docker push benboit/sw6-docker-php:7.3-cli
